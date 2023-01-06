7f90bb40240708de2eb17a47f08ab18305f8aec2
HeSer <B@nd@r1359>
2023-01-06 17:59:27 -0500

    update v1

---
+++ Day4.py
@@ -0,0 +1,24 @@
+# This is the fourth day challenge
+
+import random
+
+# r_num = random.randint(1, 6)
+# print(r_num)
+#
+# r_float = random.random()
+#
+# print(r_float)
+#
+# in_flot = (r_float + r_num) -1
+# print(in_flot)
+#
+# print(r_float *5)
+
+# r_float = random.random()
+# if (r_float *2 >= 1):
+#     print("head")
+# else:
+#     print("tail")
+#
+
+states_list = ["Delaware", "Pennsylvania"]
